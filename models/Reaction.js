fba6faa8bacb5488b063cc2ba6eab7e1923eb478
almoral323 <113931387+almoral323@users.noreply.github.com>
2023-03-09 15:57:54 -0500

    Add files via upload

---
+++ models/Reaction.js
@@ -0,0 +1,28 @@
+const { Schema, Types } = require('mongoose');
+const dateFormatter = require('../utils/dateFormatter');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            validate: [({ length }) => length <= 280, 'Reactions cannot be more than 280 characters long!']
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormatter(createdAtVal)
+        }
+    }
+)
+
+
+module.exports = ReactionSchema;
